287b5ebf2c66d92a1064d7df9f25b9e827f4f305
hardfau18 <the.qu1rky.b1t@gmail.com>
2021-02-18 16:48:43 +0530

    init and exit data

--- hello1/hello-1.c
+++ hello1/hello-1.c
@@ -1,14 +1,16 @@
 #include<linux/module.h>
 #include<linux/kernel.h>
 
+static int data[500] __initdata = {0,1};
 
-int init_module(void){
-    printk(KERN_INFO "hello-1: finally\n");
-    printk(KERN_INFO "hello-1: autocomplete working\n");
+static int __init start_here(void){
+    printk(KERN_INFO "kernel init data %d\n", data[1]);
     return 0;
 }
 
-void cleanup_module(void){
+static void __exit end_here(void){
     printk(KERN_INFO "hello-1: good bye you idiot\n");
 }
 
+module_init(start_here);
+module_exit(end_here);
