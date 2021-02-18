cba5820e1405f420a6dc424284c9aa6801925954
hardfau18 <the.qu1rky.b1t@gmail.com>
2021-02-18 17:06:55 +0530

    added some test warnings

--- hello1/hello-1.c
+++ hello1/hello-1.c
@@ -5,6 +5,7 @@ static int data[500] __initdata = {0,1};
 
 static int __init start_here(void){
     printk(KERN_INFO "kernel init data %d\n", data[1]);
+    printk(KERN_WARNING "some warnings ");
     return 0;
 }
 
