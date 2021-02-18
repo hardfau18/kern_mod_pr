bc5656021de761e0dfb926b569b4f8d681074ddb
hardfau18 <the.qu1rky.b1t@gmail.com>
2021-02-18 12:21:46 +0530

    initial

---
+++ hello1/hello-1.c
@@ -0,0 +1,13 @@
+#include<linux/module.h>
+#include<linux/kernel.h>
+
+
+int init_module(void){
+    printk(KERN_INFO "hello-1: finally\n");
+    printk(KERN_INFO "hello-1: autocomplete working\n");
+    return 0;
+}
+
+void cleanup_module(void){
+    printk(KERN_INFO "hello-1: good bye you idiot\n");
+}
